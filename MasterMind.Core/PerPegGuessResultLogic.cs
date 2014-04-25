39a5392cd83e1860f5887e2e7b64c64ef0a6a286
Stewart Anderson <stewie1570@gmail.com>
2014-04-25 14:22:03 -0600

    This logic was missing from the last check-in for some reason.

---
+++ MasterMind.Core/PerPegGuessResultLogic.cs
@@ -0,0 +1,16 @@
+﻿using MasterMind.Core.Models;
+using System.Linq;
+
+namespace MasterMind.Core
+{
+    public class PerPegGuessResultLogic : IGuessResultLogic
+    {
+        public GuessResult[] ResultFrom(GuessColor[] guess, GuessColor[] actual)
+        {
+            return guess
+                .Zip(actual, (g, a) => g == a ? GuessResult.Red : actual.Contains(g) ? GuessResult.White : GuessResult.Empty)
+                .OrderBy(r => r)
+                .ToArray();
+        }
+    }
+}
